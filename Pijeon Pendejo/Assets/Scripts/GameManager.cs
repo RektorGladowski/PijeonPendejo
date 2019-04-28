09fa729df53a074652fbd7c39f66ac60d0564646
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2019-04-28 12:19:44 +0200

    Adding destroyable pigeon at start

---
+++ Pijeon Pendejo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PigeonAtStart;
+    public GameObject BloodyExplosion;
+    public static bool GameStarted; //Turns false after first space press
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameStarted && Input.GetButtonDown("Pigeon Thrust"))
+        {
+            GameStarted = true;
+            if (BloodyExplosion)
+            {
+                Instantiate(BloodyExplosion, PigeonAtStart.transform.position, Quaternion.identity);
+            }
+            Destroy(PigeonAtStart);
+        }
+    }
+
+}
