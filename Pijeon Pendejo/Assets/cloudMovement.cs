80a1d4911407b35190182e25c0b98d911e6297c9
JJ <panjakubjakubowski@gmail.com>
2019-04-28 12:54:17 +0200

    wszystko kurwa kolorowe

--- Pijeon Pendejo/Assets/cloudMovement.cs
+++ Pijeon Pendejo/Assets/cloudMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class cloudMovement : MonoBehaviour
 {
-    public GameObject cloud1, cloud2, cloud3;
+    public GameObject cloud1, cloud2, cloud3, cloud4, cloud5, cloud6;
     public float randomX, randomY;
     // Start is called before the first frame update
     void Start()
     {
-        int choice = Random.Range(0, 3);
+        int choice = Random.Range(0, 5);
+
+
+
 
         switch (choice)
         {
@@ -23,14 +26,30 @@ public class cloudMovement : MonoBehaviour
             case 2:
                 cloud1.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
                 cloud2.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
-
-
                 break;
 
             case 3:
                 cloud1.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
                 cloud2.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
                 cloud3.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                break;
+
+            case 4:
+                cloud1.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud3.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud5.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                break;
+
+            case 5:
+                cloud2.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud4.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud5.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                break;
+
+            case 6:
+                cloud1.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud4.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+                cloud6.transform.localPosition = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
                 break;
         }
         
